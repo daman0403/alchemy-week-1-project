9def7a022c0ee6bc7c0ebedfce4283ac0b9c11bb
Daman Goyal <goyaldaman11111@gmail.com>
2023-12-10 01:07:02 +0530

    initial commit

---
+++ client/src/Wallet.jsx
@@ -0,0 +1,45 @@
+import server from "./server";
+// const secp = require("ethereum-cryptography/secp256k1");
+// const { keccak256 } = require("ethereum-cryptography/keccak")
+import * as secp from 'ethereum-cryptography/secp256k1'
+// import * as keccak256 from 'ethereum-cryptography/keccak'
+import { toHex } from 'ethereum-cryptography/utils'
+// import * as toHes from 'ethereum-cryptography/utils'
+
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+  async function onChange(evt) {
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+
+    const address= toHex(secp.secp256k1.getPublicKey(privateKey));
+
+    // const address = keccak256(publicKey.slice(1)).slice(-20);
+    setAddress(address);
+    if (address) {
+      const {
+        data: { balance },
+      } = await server.get(`balance/${address}`);
+      setBalance(balance);
+    } else {
+      setBalance(0);
+    }
+  }
+
+  return (
+    <div className="container wallet">
+      <h1>Your Wallet</h1>
+
+      <label>
+        Private Key
+        <input placeholder="Type your private key, for example: 0x1" value={privateKey} onChange={onChange}></input>
+      </label>
+      <div>
+        Address: {address}
+      </div>
+
+      <div className="balance">Balance: {balance}</div>
+    </div>
+  );
+}
+
+export default Wallet;
